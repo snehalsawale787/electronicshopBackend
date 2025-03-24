588332987fbc9b43dbb729ed796fe785368ded55
snehalsawale787 <snehalsawale787@gmail.com>
2025-03-24 17:07:17 +0530

    This is first commit

---
+++ src/main/java/com/commerce/electronicshop/ElectronicshopApplication.java
@@ -0,0 +1,14 @@
+package com.commerce.electronicshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Repository;
+
+@SpringBootApplication
+public class ElectronicshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronicshopApplication.class, args);
+	}
+
+}
